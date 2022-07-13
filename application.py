5c681c3e4082641a37818350127ed2f3aacd898e
carina <carinamoraru0520@gmail.com>
2022-07-13 09:38:52 +0800

    small change2

--- application.py
+++ application.py
@@ -1,9 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask
 application =  Flask(__name__)
-import my_bybit
-
-api_key = "cSBL3DAXa2S2Hhv5MO"
-secret_key = "QaTm9Y9DAz5aCGe3T4chjCBvV9zpN73GMbyv"
 
 @application.route('/')
 def index():
